04b9ee72ef335ae3e65bc7c569ac57bd5b666541
Shaedow2000 <shaedow2000@proton.me>
2026-05-02 13:34:57 +0100

    Refactor functions.

--- src/assets/components/buttons.ts
+++ src/assets/components/buttons.ts
@@ -1,13 +1,7 @@
-const main_button: Function = (text: string): string => {
-  return `
-    <button type="button" id="button-main">${text}</button>
-  `;
-};
+const main_button: Function = (text: string): string =>
+  `<button type="button" id="button-main">${text}</button>`;
 
-const secondary_button: Function = (text: string): string => {
-  return `
-    <button type="button" id="secondary-button">${text}</button>
-  `;
-};
+const secondary_button: Function = (text: string): string =>
+  `<button type="button" id="secondary-button">${text}</button>`;
 
 export { main_button, secondary_button };
