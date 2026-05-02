27f3287b44221bcdfcd4c6b7de4601d7d5abd7f3
Shaedow2000 <shaedow2000@proton.me>
2026-05-02 13:32:13 +0100

    Create secondary button component.

--- src/assets/components/buttons.ts
+++ src/assets/components/buttons.ts
@@ -2,4 +2,8 @@ const main_button: string = /* html */ `
   <button type="button" id="button-main">Button</button>
 `;
 
-export { main_button };
+const secondary_button: string = /* html */ `
+  <button type="button" id="secondary-button">Button</button>
+`;
+
+export { main_button, secondary_button };
