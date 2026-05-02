56e2ceaf997b56db7d8be5842c6bc79aac850982
Shaedow2000 <shaedow2000@proton.me>
2026-05-02 16:55:25 +0100

    Create hero section component.

---
+++ src/assets/components/hero.ts
@@ -0,0 +1,11 @@
+const hero: string = `
+  <section class="hero">
+    <img src="src/assets/SVG/Logo.svg" alt="Thinc-logo" />
+    <div>
+      <p>Your Ideas.</p>
+      <p>Organized.</p>
+    </div>
+  </section>
+`;
+
+export { hero };
