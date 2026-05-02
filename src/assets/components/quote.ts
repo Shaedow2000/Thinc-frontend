adac0e949acd4d29626b2b59c4fe0a5bbebd3c33
Shaedow2000 <shaedow2000@proton.me>
2026-05-02 17:01:53 +0100

    Add text block svg & Create quote section component.

---
+++ src/assets/components/quote.ts
@@ -0,0 +1,8 @@
+const quote: string = `
+  <section class="quote">
+    <img src="src/assets/SVG/Text block.svg" alt="Text block" />
+    <p>Open-source solution to your problems</p>
+  </section>
+`;
+
+export { quote };
