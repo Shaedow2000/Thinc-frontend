e87a22fa39612f9946d0b19e70c3327593bdab82
Shaedow2000 <shaedow2000@proton.me>
2026-05-02 15:10:39 +0100

    Create footer component

---
+++ src/assets/components/footer.ts
@@ -0,0 +1,13 @@
+const footer: string = /* html */ `
+  <footer id="footer">
+    <div>
+      <div>
+        <img src="src/assets/SVG/Logo.svg" alt="Thinc-logo" />
+        <p>copyright © 2026.</p>
+      </div>
+      <p>thinc-dev@zohomail.com</p>
+    </div>
+  </footer>
+`;
+
+export { footer };
