c39779ec69bf38e29916d3272e312ecaad4f58fc
Shaedow2000 <shaedow2000@proton.me>
2026-05-02 13:09:09 +0100

    Add all svgs & create header with the svgs

---
+++ src/assets/components/header.ts
@@ -0,0 +1,13 @@
+const header_svgs: string = /* html */ `
+  <header id="header">
+    <div>
+      <img src="src/assets/SVG/Logo.svg" alt="Thinc-Logo" />
+      <div>
+        <img src="src/assets/SVG/Avatar.svg" alt="Avatar" id="svg" />
+        <img src="src/assets/SVG/Menu.svg" alt="menu" id="svg" />
+      </div>
+    </div>
+  </header>
+`;
+
+export { header_svgs };
