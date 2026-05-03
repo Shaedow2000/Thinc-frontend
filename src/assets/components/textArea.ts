a99039498812479b12c57064031ce18a98ebc40f
Shaedow2000 <shaedow2000@proton.me>
2026-05-03 16:37:33 +0100

    Create text area component.

---
+++ src/assets/components/textArea.ts
@@ -0,0 +1,9 @@
+const textArea: string = `
+  <section class="textarea">
+    <div class="separator"></div>
+    <textarea name="text" placeholder="Your note here!"></textarea>
+    <div class="separator"></div>
+  </section>
+`;
+
+export { textArea };
