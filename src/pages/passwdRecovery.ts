f8a1d0819b932b9fc1496f0b7d8bb8185807d33e
Shaedow2000 <shaedow2000@proton.me>
2026-05-03 22:01:07 +0100

    Add functionallity to password recovery.

--- src/pages/passwdRecovery.ts
+++ src/pages/passwdRecovery.ts
@@ -23,6 +23,25 @@ const passwdRecoveryPage: Function = (): void => {
       </form> 
     </div>
   `;
+
+  const recoveryForm = document.getElementById(
+    "recovery-form",
+  ) as HTMLFormElement;
+
+  recoveryForm.addEventListener(
+    "submit",
+    async (e: SubmitEvent): Promise<void> => {
+      e.preventDefault();
+
+      let response = await fetch("http://localhost:8080/auth/password_reset", {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(Object.fromEntries(new FormData(recoveryForm))),
+      });
+
+      let result = await response.json();
+    },
+  );
 };
 
 export default passwdRecoveryPage;
