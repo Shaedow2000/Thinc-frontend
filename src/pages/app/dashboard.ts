c16ed6e700e64c99499e963bea2420458adf9a34
Shaedow2000 <shaedow2000@proton.me>
2026-05-06 17:49:29 +0100

    Add error handler for wrong json object stored in session storage

--- src/pages/app/dashboard.ts
+++ src/pages/app/dashboard.ts
@@ -4,13 +4,22 @@ import { headerSvgs } from "../../assets/components/header";
 import { quickActions } from "../../assets/components/quickActions";
 import { recentNotesSection } from "../../assets/components/recentNotes";
 import { pageTitle } from "../../assets/components/text";
+import isUserLoggedIn from "../../utils/userLoggedIn";
 
-const dashboardPage: Function = (): void => {
+const dashboardPage: Function = async (): Promise<void> => {
   const app = document.getElementById("app") as HTMLDivElement;
 
   app.style.height = "fit-content";
 
-  let notes: [] = JSON.parse(sessionStorage.getItem("notes") ?? "") ?? [];
+  let notes: [] = [];
+
+  try {
+    notes = JSON.parse(sessionStorage.getItem("notes") ?? "") ?? [];
+  } catch (err: Error | unknown) {
+    sessionStorage.clear();
+    await isUserLoggedIn();
+    notes = JSON.parse(sessionStorage.getItem("notes") ?? "") ?? [];
+  }
 
   app.innerHTML = `
     ${pageTitle("Dashboard")}
