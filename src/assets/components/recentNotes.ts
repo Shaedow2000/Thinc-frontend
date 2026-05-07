1debb8bbdb172666f105611088b0977e10324628
Shaedow2000 <shaedow2000@proton.me>
2026-05-07 16:00:08 +0100

    Fix responsiveness for smaller devices than the iphone se.

--- src/assets/components/recentNotes.ts
+++ src/assets/components/recentNotes.ts
@@ -4,7 +4,7 @@ import { cardsSection } from "./cardsSection";
 const recentNotesSection: Function = (notes: [] = []): string => `
   <section class="recent-notes">
     ${heading("Recent notes")}
-    <div style="display: ${notes.length !== 0 ? "block" : "none"}">${cardsSection(notes)}</div>
+    <div class="w-full" style="display: ${notes.length !== 0 ? "block" : "none"}">${cardsSection(notes)}</div>
     <div class="text-center text-text text-md" style="display: ${notes.length !== 0 ? "none" : "block"}">No notes yet!</div>
   </section>
 `;
