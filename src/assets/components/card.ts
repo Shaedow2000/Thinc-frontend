47ef7b8b78b479fe2cc3b5012aad21da109f9cb2
Shaedow2000 <shaedow2000@proton.me>
2026-05-07 14:25:54 +0100

    Add limit on how many characters are showed in the card section.

--- src/assets/components/card.ts
+++ src/assets/components/card.ts
@@ -2,13 +2,25 @@ const card: Function = (
   title: string,
   text: string,
   noteId: string,
-): string => `
-  <section class="card cursor-pointer" id="${noteId}">
-    <div class="card-div">
-      <h2 class="card-h2">${title}</h2>
-      <p class="card-p">${text}</p>
-    </div>
-  </section>
-`;
+): string => {
+  if (title.length >= 14) {
+    title = title.slice(0, 13);
+    title = `${title}...`;
+  }
+
+  if (text.length >= 60) {
+    text = text.slice(0, 56);
+    text = `${text}...`;
+  }
+
+  return `
+    <section class="card cursor-pointer" id="${noteId}">
+      <div class="card-div">
+        <h2 class="card-h2">${title}</h2>
+        <p class="card-p">${text}</p>
+      </div>
+    </section>
+  `;
+};
 
 export { card };
